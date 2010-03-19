a23cc9e3317bdc0d9ec449f77a49acd86bbd2409
Coda Hale <coda.hale@gmail.com>
2010-03-19 15:26:46 -0700

    Mark mockito as a test-only dependency.

--- project/build/ScromiumProject.scala
+++ project/build/ScromiumProject.scala
@@ -6,9 +6,10 @@ class ScromiumProject(info : ProjectInfo) extends DefaultProject(info) with Basi
   
   val metrics = "com.yammer" % "metrics_2.8.0.Beta1" % "1.0.2" withSources()
   val guild = "com.codahale" % "guild_2.8.0.Beta1" % "1.0-SNAPSHOT" withSources()
-  val mockito = "org.mockito" % "mockito-all" % "1.8.1" withSources()
   val jetlang = "org.jetlang" % "jetlang" % "0.2.0" withSources()
   val pool = "commons-pool" % "commons-pool" % "1.5.4" withSources() intransitive()
   val slf4japi = "org.slf4j" % "slf4j-api" % "1.5.11" withSources() intransitive()
   val slf4j = "org.slf4j" % "slf4j-jdk14" % "1.5.11" withSources() intransitive()
+  
+  val mockito = "org.mockito" % "mockito-all" % "1.8.1" % "test" withSources()
 }

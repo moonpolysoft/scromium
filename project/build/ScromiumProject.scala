428f39baab99c1302c2b686e257e69c9817653a3
Coda Hale <coda.hale@gmail.com>
2010-03-19 15:24:23 -0700

    Bump slf4j to 1.5.11.

--- project/build/ScromiumProject.scala
+++ project/build/ScromiumProject.scala
@@ -10,8 +10,8 @@ class ScromiumProject(info : ProjectInfo) extends DefaultProject(info) with Basi
   val jetlang = "org.jetlang" % "jetlang" % "0.2.0" withSources()
   val pool = "commons-pool" % "commons-pool" % "1.5.2" withSources()
 /*  val specs = "org.scala-tools.testing" % "specs_2.8.0.Beta1" % "1.6.3" withSources()*/
-  val slf4japi = "org.slf4j" % "slf4j-api" % "1.5.2"
-  val slf4j = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
+  val slf4japi = "org.slf4j" % "slf4j-api" % "1.5.11" withSources() intransitive()
+  val slf4j = "org.slf4j" % "slf4j-jdk14" % "1.5.11" withSources() intransitive()
  /*  val cassandra = "org.apache.cassandra" % "apache-cassandra" % "0.6.0-beta2" withSources()
   val thrift = "org.apache.thrift" % "libthrift" % "r917130" withSources()
   val specs = */

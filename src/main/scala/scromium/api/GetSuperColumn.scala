81478b4298acd7a48b56d57bc58d341cc38aff30
Cliff Moon <cliff@moonpolysoft.com>
2010-03-21 21:35:33 -0700

    turn getsupercolumn into a case class

--- src/main/scala/scromium/api/GetSuperColumn.scala
+++ src/main/scala/scromium/api/GetSuperColumn.scala
@@ -4,13 +4,15 @@ import org.apache.cassandra.thrift
 import scromium.serializers._
 import scala.collection.JavaConversions._
 
-class GetSuperColumn(superColumn : thrift.SuperColumn) extends Iterable[GetColumn] {
+case class GetSuperColumn(val name : Array[Byte], val columns : Seq[GetColumn]) extends Iterable[GetColumn] {
+  def this(superColumn : thrift.SuperColumn) {
+    this(superColumn.name, superColumn.columns.map(new GetColumn(_)))
+  }
+  
   def this(result : thrift.ColumnOrSuperColumn) {
     this(result.super_column)
   }
-  var columns = superColumn.columns.map(new GetColumn(_))
   
-  def name = superColumn.name
   def nameAs[T](deserializer : Deserializer[T]) = deserializer.deserialize(name)
   def iterator = columns.iterator
 }

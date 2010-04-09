f10752e34ac6d992da97b2d4fe41ef5044374b64
Cliff Moon <cliff@moonpolysoft.com>
2010-04-08 22:46:15 -0700

    fuck a thrift

--- src/main/scala/scromium/util/Thrift.scala
+++ src/main/scala/scromium/util/Thrift.scala
@@ -1,6 +1,7 @@
 package scromium.util
 
 import org.apache.cassandra.thrift._
+import scala.collection.JavaConversions._
 
 object Thrift {
   def columnContainer(c : Array[Byte], value : Array[Byte], timestamp : Long) : ColumnOrSuperColumn = {
@@ -33,5 +34,34 @@ object Thrift {
     val container = superColumnContainer(sc,columns : _*)
     mutation.column_or_supercolumn = container
     mutation
+  }
+  
+  def keyRange(startKey : String, endKey : String, count : Int) : KeyRange = {
+    val kr = new KeyRange
+    kr.start_key = startKey
+    kr.end_key = endKey
+    kr.count = count
+    kr
+  }
+  
+  def sliceRange(start : Array[Byte], end : Array[Byte], count : Int) : SliceRange = {
+    val r = new SliceRange
+    r.start = start
+    r.finish = end
+    r.count = count
+    r
+  }
+  
+  def slicePredicate(start : Array[Byte], end : Array[Byte], count : Int) : SlicePredicate = {
+    val p = new SlicePredicate
+    p.slice_range = sliceRange(start, end, count)
+    p
+  }
+  
+  def slicePredicate(columns : Array[Byte]*) : SlicePredicate = {
+    val p = new SlicePredicate
+    p.column_names = new java.util.ArrayList[Array[Byte]]
+    columns.foreach { c => p.column_names.add(c) }
+    p
   }
 }

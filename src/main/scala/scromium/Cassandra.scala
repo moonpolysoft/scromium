1f6e86714e8e48a994abdf37d39ee6158b2c4cc8
Cliff Moon <cliff@moonpolysoft.com>
2010-04-09 23:33:08 -0700

    an embedded version of scala that can be used from within tests

--- src/main/scala/scromium/Cassandra.scala
+++ src/main/scala/scromium/Cassandra.scala
@@ -5,11 +5,26 @@ import scromium.reflect.Reflect._
 import scromium.util.Log
 import java.io._
 import scromium.util.JSON
+import org.apache.cassandra.config.DatabaseDescriptor
+import org.apache.cassandra.db.CompactionManager
+import org.apache.cassandra.db.Table
+import org.apache.cassandra.io.util.FileUtils
+import org.apache.cassandra.db.commitlog.CommitLog
+import scala.collection.JavaConversions._
+import org.apache.cassandra.service.StorageService
+import org.apache.cassandra.net.MessagingService
+import org.apache.cassandra.thrift.CassandraServer
+import java.lang.management.ManagementFactory
+import javax.management._
 
 object Cassandra extends Log {
   def start(file : String) : Cassandra = {
     val config = getConfig(new File(file))
     start(config)
+  }
+  
+  def start(map : Map[String, Any]) : Cassandra = {
+    new Cassandra(createConnectionPool(map))
   }
   
   def start() : Cassandra = {    
@@ -20,8 +35,30 @@ object Cassandra extends Log {
     start(config)
   }
   
-  def start(map : Map[String, Any]) : Cassandra = {
-    new Cassandra(createConnectionPool(map))
+  def startTest() : Cassandra = {
+    try {
+      DatabaseDescriptor.createAllDirectories
+      for (table <- DatabaseDescriptor.getTables) {
+        println("opening table " + table)
+          Table.open(table)
+      }
+    
+/*      CommitLog.recover
+      CompactionManager.instance.checkAllColumnFamilies*/
+      StorageService.instance.initServer
+      val server = new CassandraServer
+      val pool =  new ConnectionPool {
+        def withConnection[T](block : Client => T) : T = {
+          block(new TestClient(server))
+        }
+      }
+      new Cassandra(pool)
+    } catch {
+      case e : Throwable =>
+        println("fuck " + e.getClass)
+        e.getCause.printStackTrace
+        throw e
+    }
   }
   
   private val default = Map("host" -> "localhost", 
@@ -71,4 +108,32 @@ object Cassandra extends Log {
 
 class Cassandra(connPool : ConnectionPool) {
   def keyspace(name : String) = new Keyspace(name, connPool)
+  
+  def teardownTest() {
+    
+    MessagingService.shutdown()
+    for (table <- DatabaseDescriptor.getTables) {
+      println("closing table " + table)
+        Table.close(table)
+    }
+    
+    val server = ManagementFactory.getPlatformMBeanServer
+    val query = new QueryExp {
+      def apply(name : ObjectName) : Boolean = {
+        name.getDomain.startsWith("org.apache.cassandra")
+      }
+      
+      def setMBeanServer(s : MBeanServer) {
+        
+      }
+    }
+    for (name <- server.queryNames(null,query).asInstanceOf[java.util.Set[ObjectName]]) {
+      server.unregisterMBean(name)
+    }
+    
+    FileUtils.deleteDir(new File(DatabaseDescriptor.getLogFileLocation))
+    for (dir <- DatabaseDescriptor.getAllDataFileLocations) {
+      FileUtils.deleteDir(new File(dir))
+    }
+  }
 }

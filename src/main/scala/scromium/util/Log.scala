c1026aac7142760956f6d9272c5a725ba56f177b
Cliff Moon <cliff@moonpolysoft.com>
2010-04-12 17:03:43 -0700

    rework the logging interface

--- src/main/scala/scromium/util/Log.scala
+++ src/main/scala/scromium/util/Log.scala
@@ -3,35 +3,30 @@ package scromium.util
 import org.slf4j.{Logger, LoggerFactory}
 
 trait Log {
- private val log = LoggerFactory.getLogger(getClass)
+  private val log = LoggerFactory.getLogger(getClass)
 
- def trace(message:String, values:Any*) =
-     log.trace(message, values.map(_.asInstanceOf[Object]).toArray)
- def trace(message:String, error:Throwable) = log.trace(message, error)
- def trace(message:String, error:Throwable, values:Any*) =
-     log.trace(message, error, values.map(_.asInstanceOf[Object]).toArray)
-
- def debug(message:String, values:Any*) =
-     log.debug(message, values.map(_.asInstanceOf[Object]).toArray)
- def debug(message:String, error:Throwable) = log.debug(message, error)
- def debug(message:String, error:Throwable, values:Any*) =
-     log.debug(message, error, values.map(_.asInstanceOf[Object]).toArray)
-
- def info(message:String, values:Any*) =
-     log.info(message, values.map(_.asInstanceOf[Object]).toArray)
- def info(message:String, error:Throwable) = log.info(message, error)
- def info(message:String, error:Throwable, values:Any*) =
-     log.info(message, error, values.map(_.asInstanceOf[Object]).toArray)
-
- def warn(message:String, values:Any*) =
-     log.warn(message, values.map(_.asInstanceOf[Object]).toArray)
- def warn(message:String, error:Throwable) = log.warn(message, error)
- def warn(message:String, error:Throwable, values:Any*) =
-     log.warn(message, error, values.map(_.asInstanceOf[Object]).toArray)
-
- def error(message:String, values:Any*) =
-     log.error(message, values.map(_.asInstanceOf[Object]).toArray)
- def error(message:String, error:Throwable) = log.error(message, error)
- def error(message:String, error:Throwable, values:Any*) =
-     log.error(message, error, values.map(_.asInstanceOf[Object]).toArray)
+  def trace(message : => String) =
+    if (log.isTraceEnabled) log.trace(message)
+  def trace(message : => String, error:Throwable) =
+    if (log.isTraceEnabled) log.trace(message, error)
+    
+  def debug(message : => String) =
+    if (log.isDebugEnabled) log.debug(message)
+  def debug(message : => String, error:Throwable) =
+    if (log.isDebugEnabled) log.debug(message, error)
+    
+  def info(message : => String) =
+    if (log.isInfoEnabled) log.info(message)
+  def info(message : => String, error:Throwable) =
+    if (log.isInfoEnabled) log.info(message, error)
+    
+  def warn(message : => String) =
+    if (log.isWarnEnabled) log.warn(message)
+  def warn(message : => String, error:Throwable) =
+    if (log.isWarnEnabled) log.warn(message, error)
+    
+  def error(message : => String) =
+    if (log.isErrorEnabled) log.error(message)
+  def error(message : => String, error:Throwable) =
+    if (log.isErrorEnabled) log.error(message, error)
 }

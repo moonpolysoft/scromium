cf9cb6b90a0644149defe31d7a6ed3320ed3b6cd
Coda Hale <coda.hale@gmail.com>
2010-04-19 14:42:08 -0700

    Revert "Stop building docs, since they explode."
    This reverts commit aad4a527766a078c877a95cfd39d1aa50b916a46.

--- project/build/ScromiumProject.scala
+++ project/build/ScromiumProject.scala
@@ -4,7 +4,8 @@ class ScromiumProject(info : ProjectInfo) extends DefaultProject(info) with Basi
   override def compileOptions = Deprecation :: Unchecked :: super.compileOptions.toList
   
   val sourceArtifact = Artifact(artifactID, "src", "jar", Some("sources"), Nil, None) 
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
+  val docsArtifact = Artifact(artifactID, "docs", "jar", Some("javadocs"), Nil, None) 
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
   
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
   val jetlangRepo = "Jet Lang Repository" at "http://jetlang.googlecode.com/svn/repo/"

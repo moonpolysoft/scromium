0002eaaa02fcedd5f9c9389bbc4bfa1e40267a31
Cliff Moon <cliff@moonpolysoft.com>
2010-08-26 10:49:28 -0700

    client logging

--- src/main/scala/scromium/thrift/ThriftClient.scala
+++ src/main/scala/scromium/thrift/ThriftClient.scala
@@ -3,7 +3,7 @@ package scromium.thrift
 import scromium._
 import scromium.meta._
 import scromium.client._
-import scromium.util.{DefaultHashMap, ArrayKeyedHashMap}
+import scromium.util.{DefaultHashMap, ArrayKeyedHashMap, Log}
 import org.apache.cassandra.thrift
 import org.apache.thrift.transport.{TTransport, TTransportException}
 import java.util.{Map => JMap, List => JList}
@@ -26,7 +26,7 @@ class ThriftConnection(socket : TTransport, client : thrift.Cassandra.Client) ex
   }
 }
 
-class ThriftClient(cass : thrift.Cassandra.Iface) extends Client {
+class ThriftClient(cass : thrift.Cassandra.Iface) extends Client with Log {
   type MuteMap = JMap[String, JList[thrift.Mutation]]
   
   def put(keyspace : String, writes : List[Write[Column]], c : WriteConsistency) {
@@ -36,6 +36,7 @@ class ThriftClient(cass : thrift.Cassandra.Iface) extends Client {
       val mutes = rowMap(write.key)(write.cf)
       mutes ++= write.columns.map(columnMutation(_))
     }
+    debug("batch_mutate(" + rowMap + "," + c.thrift + ")")
     cass.batch_mutate(rowMap, c.thrift)
   }
   
@@ -46,6 +47,7 @@ class ThriftClient(cass : thrift.Cassandra.Iface) extends Client {
       val mutes = rowMap(write.key)(write.cf)
       mutes ++= write.columns.map(superColumnMutation(_))
     }
+    debug("batch_mutate(" + rowMap + "," + c.thrift + ")")
     cass.batch_mutate(rowMap, c.thrift)
   }
   
@@ -56,6 +58,7 @@ class ThriftClient(cass : thrift.Cassandra.Iface) extends Client {
     delete.keys.foreach { key =>
       rowMap(key)(delete.cf) += mutation
     }
+    debug("batch_mutate(" + rowMap + "," + c.thrift + ")")
     cass.batch_mutate(rowMap, c.thrift)
   }
   
@@ -63,6 +66,7 @@ class ThriftClient(cass : thrift.Cassandra.Iface) extends Client {
     cass.set_keyspace(keyspace)
     val parent = readToColumnParent(read)
     val predicate = readToPredicate(read)
+    debug("multiget_slice(" + read.keys + "," + parent + "," + predicate + "," + c.thrift + ")")
     val results = cass.multiget_slice(read.keys, parent, predicate, c.thrift)
     new MGColumnRowIterator(results)
   }
@@ -71,35 +75,43 @@ class ThriftClient(cass : thrift.Cassandra.Iface) extends Client {
     cass.set_keyspace(keyspace)
     val parent = readToColumnParent(read)
     val predicate = readToPredicate(read)
+    debug("multiget_slice(" + read.keys + "," + parent + "," + predicate + "," + c.thrift + ")")
     val results = cass.multiget_slice(read.keys, parent, predicate, c.thrift)
     new MGSuperColumnRowIterator(results)
   }
   
   def createKeyspace(keyspace : KeyspaceDef) {
+    debug("system_add_keyspace(" + ksDef(keyspace) + ")")
     cass.system_add_keyspace(ksDef(keyspace))
   }
   
   def createColumnFamily(cf : ColumnFamilyDef) {
+    debug("system_add_column_family(" + cfDef(cf) + ")")
     cass.system_add_column_family(cfDef(cf))
   }
   
   def dropKeyspace(name : String) {
+    debug("system_drop_keyspace(" + name + ")")
     cass.system_drop_keyspace(name)
   }
   
   def renameKeyspace(from : String, to : String) {
+    debug("system_rename_keyspace(" + from + "," + to + ")")
     cass.system_rename_keyspace(from, to)
   }
   
   def dropColumnFamily(name : String) {
+    debug("system_drop_column_family(" + name + ")")
     cass.system_drop_column_family(name)
   }
   
   def renameColumnFamily(from : String, to : String) {
+    debug("system_rename_column_family(" + from + "," + to + ")")
     cass.system_rename_column_family(from, to)
   }
   
   def listKeyspaces : Set[String] = {
+    debug("describe_keyspaces()")
     Set(cass.describe_keyspaces.toSeq : _*)
   }
   

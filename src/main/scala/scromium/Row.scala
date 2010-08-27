81759b844da0005e96ea2d9cca939180e92856c2
Cliff Moon <cliff@moonpolysoft.com>
2010-08-26 22:00:02 -0700

    method to deserialize row keys

--- src/main/scala/scromium/Row.scala
+++ src/main/scala/scromium/Row.scala
@@ -3,5 +3,7 @@ package scromium
 import serializers._
 
 abstract class Row[T <: Columnar](val key : Array[Byte]) {
+  def keyAs[K](implicit ser : Deserializer[K]) = ser.deserialize(key)
+  
   def columns : Iterator[T]
 }

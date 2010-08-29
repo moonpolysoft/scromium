5d9900db2d1255ab9543626bee417f11a393a9cc
Cliff Moon <cliff@moonpolysoft.com>
2010-08-28 23:20:31 -0700

    wrap the client provider to inject the runtime dependencies

--- src/main/scala/scromium/thrift/ThriftClientProvider.scala
+++ src/main/scala/scromium/thrift/ThriftClientProvider.scala
@@ -3,7 +3,15 @@ package scromium.thrift
 import scromium.client._
 import scromium.util.Log
 
-class ThriftClientProvider(objectPool : ThriftClientPool) extends ClientProvider with Log {
+class ThriftClientProvider(options : Map[String, String]) extends ClientProvider {
+  val provider = new WrappedProvider(new ThriftClientPool(options,
+    new ThriftSocketFactory,
+    new ThriftClusterDiscovery))
+  
+  def withClient[T](block : Client => T) : T = provider.withClient(block)
+}
+
+class WrappedProvider(objectPool : ThriftClientPool) extends ClientProvider with Log {
   
   def withClient[T](block : Client => T) : T = {
     var connection : ThriftClient = null

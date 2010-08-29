87c49ceb9caca6378edad34637b9930375c6afbc
Cliff Moon <cliff@moonpolysoft.com>
2010-08-28 23:13:58 -0700

    add a timer task to the thrift client pool so it will periodically update the host list from the seed node

--- src/main/scala/scromium/thrift/ThriftClientPool.scala
+++ src/main/scala/scromium/thrift/ThriftClientPool.scala
@@ -4,6 +4,7 @@ import org.apache.commons.pool._
 import org.apache.commons.pool.impl._
 import scromium.util.JSON
 import scromium.util.Log
+import java.util.{Timer, TimerTask}
 
 
 class ThriftClientPool(config : Map[String,Any],
@@ -14,9 +15,26 @@ class ThriftClientPool(config : Map[String,Any],
   val seedPort = config("seedPort").asInstanceOf[Int]
   val maxIdle = config("maxIdle").asInstanceOf[Int]
   val initCapacity = config("initCapacity").asInstanceOf[Int]
-  
   val hosts = clusterDiscovery.hosts(seedHost,seedPort)
-  val p = new GenericObjectPool(new ThriftClientFactory(hosts, seedPort, socketFactory))
+  val clientFactory = new ThriftClientFactory(hosts, seedPort, socketFactory)
+  
+  val hostRefreshTask = new TimerTask {
+    override def run {
+      try {
+        val newHosts = clusterDiscovery.hosts(seedHost, seedPort)
+        clientFactory.synchronized {
+          clientFactory.hosts = newHosts
+        }
+      } catch {
+        case e : Throwable =>
+          error("Caught exception trying to update the endpoint list.", e)
+      }
+    }
+  }
+  val hostRefreshTimer = new Timer("host-refresh-timer")
+  hostRefreshTimer.schedule(hostRefreshTask, 60000, 60000)
+  
+  val p = new GenericObjectPool(clientFactory)
   p.setMaxIdle(maxIdle)
   
   def borrow : ThriftClient = {
@@ -29,5 +47,9 @@ class ThriftClientPool(config : Map[String,Any],
     synchronized {
       p.returnObject(conn)
     }
+  }
+  
+  override def finalize {
+    hostRefreshTimer.cancel
   }
 }
